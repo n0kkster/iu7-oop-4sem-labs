d7a1069ebe6e78ad77c167fd0503772780407cae
n0kkster <order.graph@gmail.com>
2025-03-21 14:34:13 +0300

    upd: перенес файлы ноды в отдельную папочку

---
+++ lab_02/inc/node/set_node.h
@@ -0,0 +1,16 @@
+#ifndef SET_NODE_H
+#define SET_NODE_H
+
+class SetNode
+{
+
+private:
+    /* data */
+
+public:
+    SetNode(/* args */);
+    ~SetNode();
+    
+};
+
+#endif /* SET_NODE_H */
---
+++ lab_02/inc/node/set_node.hpp
@@ -0,0 +1,5 @@
+#ifndef SET_NODE_HPP
+#define SET_NODE_HPP
+
+
+#endif /* SET_NODE_HPP */
