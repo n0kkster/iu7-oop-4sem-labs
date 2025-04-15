d2373055803147124282c91ec34c3df2233e8f20
n0kkster <order.graph@gmail.com>
2025-04-15 23:42:45 +0300

    fix: убрал ненужные перегруженные методы

--- lab_02/inc/exception/exception.h
+++ lab_02/inc/exception/exception.h
@@ -7,24 +7,18 @@ class IteratorException : BaseException
 {
 public:
     IteratorException(const char *info, const std::source_location &loc = std::source_location::current());
-
-    const char *what() const noexcept override;
 };
 
 class SetException : BaseException
 {
 public:
     SetException(const char *info, const std::source_location &loc = std::source_location::current());
-
-    const char *what() const noexcept override;
 };
 
 class MemoryException : BaseException
 {
 public:
     MemoryException(const char *info, const std::source_location &loc = std::source_location::current());
-
-    const char *what() const noexcept override;
 };
 
 #include "exception.hpp"
