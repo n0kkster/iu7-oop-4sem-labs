385bf5f45b2990ae43e7cac299d9c5701e613198
n0kkster <order.graph@gmail.com>
2025-04-15 23:47:38 +0300

    fix: поправил пару ошибочек

--- lab_02/inc/iterator/const_iterator.hpp
+++ lab_02/inc/iterator/const_iterator.hpp
@@ -1,6 +1,6 @@
-#ifndef CONST_ITERATOR_HPP
-#define CONST_ITERATOR_HPP
+#pragma once
 
+#include "exception.h"
 #include "const_iterator.h"
 
 template <typename Type>
@@ -12,7 +12,7 @@ ConstIterator<Type>::ConstIterator()
 template <typename Type>
 ConstIterator<Type>::ConstIterator(const std::shared_ptr<SetNode<Type>> &pnode)
 {
-    this.curr = pnode;
+    this->curr = pnode;
 }
 
 template <typename Type>
@@ -45,7 +45,7 @@ ConstIterator<Type> &ConstIterator<Type>::operator++()
     checkExpired(__LINE__);
 
     next();
-    return this;
+    return *this;
 }
 
 template <typename Type>
@@ -101,6 +101,7 @@ ConstIterator<Type> ConstIterator<Type>::operator+(int n) const
     auto copy = this;
     while (n --> 0)
         ++copy;
+
     return copy;
 }
 
@@ -109,8 +110,8 @@ ConstIterator<Type> &ConstIterator<Type>::operator+=(int n)
 {
     checkExpired(__LINE__);
 
-    while (n --> 0)
-        ++this;
+    this = this + n;
+
     return this;
 }
 
@@ -122,6 +123,7 @@ ConstIterator<Type> ConstIterator<Type>::operator-(int n) const
     auto copy = this;
     while (n --> 0)
         --copy;
+
     return copy;
 }
 
@@ -130,8 +132,8 @@ ConstIterator<Type> &ConstIterator<Type>::operator-=(int n)
 {
     checkExpired(__LINE__);
 
-    while (n --> 0)
-        --this;
+    this = this - n;
+    
     return this;
 }
 
@@ -196,12 +198,9 @@ bool ConstIterator<Type>::operator!=(const ConstIterator<Type> &other) const
     return this->curr.lock() != other.curr.lock();
 }
 
-#include <stdio.h>
 template <typename Type>
 void ConstIterator<Type>::checkExpired(int line) const
 {
-    printf("%d\n", line);
-    abort();
+    if (this->curr.expired() && this->curr.lock())
+        throw new IteratorException("Iterator pointer expired.");
 }
-
-#endif /* CONST_ITERATOR_H */
