c5b2bcf399a1c90cc774035ac4efbdaa09bec010
n0kkster <order.graph@gmail.com>
2025-04-15 16:46:22 +0300

    add: добавил несколько исключений

--- lab_02/inc/exception/exception.h
+++ lab_02/inc/exception/exception.h
@@ -1,3 +1,30 @@
 #pragma once
 
 #include "base_exception.h"
+#include <source_location>
+
+class IteratorException : BaseException
+{
+public:
+    IteratorException(const char *info, const std::source_location &loc = std::source_location::current());
+
+    const char *what() const noexcept override;
+};
+
+class SetException : BaseException
+{
+public:
+    SetException(const char *info, const std::source_location &loc = std::source_location::current());
+
+    const char *what() const noexcept override;
+};
+
+class MemoryException : BaseException
+{
+public:
+    MemoryException(const char *info, const std::source_location &loc = std::source_location::current());
+
+    const char *what() const noexcept override;
+};
+
+#include "exception.hpp"
---
+++ lab_02/inc/exception/exception.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "exception.h"
+
+IteratorException::IteratorException(const char *info, const std::source_location &loc) :
+    BaseException(info, loc) { };
+
+SetException::SetException(const char *info, const std::source_location &loc) : 
+    BaseException(info, loc) { };
+
+MemoryException::MemoryException(const char *info, const std::source_location &loc) :
+    BaseException(info, loc) { };
