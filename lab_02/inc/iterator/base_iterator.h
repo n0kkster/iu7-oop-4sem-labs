1d2feadd66a91a4efb80ddeddbaa11687ef84434
n0kkster <order.graph@gmail.com>
2025-04-15 23:44:43 +0300

    add: добавил реализацию деструктура для баового итератора

--- lab_02/inc/iterator/base_iterator.h
+++ lab_02/inc/iterator/base_iterator.h
@@ -1,14 +1,18 @@
 #pragma once
 
-#include <memory>
 #include "set_node.h"
+
+#include <memory>
 
 template <typename Type>
 class BaseIterator
 {
 public:
+    BaseIterator() = default;
     virtual ~BaseIterator() = 0;
-    
+
 protected:
     std::weak_ptr<SetNode<Type>> curr;
 };
+
+#include "base_iterator.hpp"
---
+++ lab_02/inc/iterator/base_iterator.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "base_iterator.h"
+
+template <typename Type>
+BaseIterator<Type>::~BaseIterator() { }
