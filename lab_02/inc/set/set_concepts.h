92d3257efa25d845abce1d1206f1d9bfa6c39e04
n0kkster <order.graph@gmail.com>
2025-04-18 12:59:52 +0300

    add: какие-то концептики добавил

--- lab_02/inc/set/set_concepts.h
+++ lab_02/inc/set/set_concepts.h
@@ -2,16 +2,20 @@
 
 #include <compare>
 #include <concepts>
+#include <iterator>
 
 template <typename From, typename To>
 concept Convertible = std::convertible_to<From, To>;
 
-template <typename Type>
-concept Assignable = requires(Type &lv, Type &rv) { lv = rv; };
+template <typename T>
+concept Assignable = requires(T &lv, T &rv) { lv = rv; };
 
-template <typename Type>
+template <typename T>
 concept CopyMoveAssignable =
-    std::copy_constructible<Type> && std::move_constructible<Type> && Assignable<Type>;
+    std::copy_constructible<T> && std::move_constructible<T> && Assignable<T>;
+
+template<typename T, typename U>
+    concept EqualityComparable = std::equality_comparable_with<T, U>;
 
 template <typename C>
 concept CopyConstructible = requires(const C &c) {
@@ -34,3 +38,24 @@ concept Container =
         { c.size() } noexcept -> std::same_as<typename C::size_type>;
         { c.empty() } noexcept -> std::same_as<bool>;
     };
+
+template <typename Container>
+    concept Comparable = requires(Container cont1, Container cont2) {
+        { cont1 == cont2 } noexcept -> std::same_as<bool>;
+        { cont1 != cont2 } noexcept -> std::same_as<bool>;
+};
+
+template <typename It>
+    concept BidirectionalIterator = std::bidirectional_iterator<It>;
+    
+template <typename It, typename T>
+    concept ConvertibleInputIterator = BidirectionalIterator<It> && Convertible<typename It::value_type, T>;
+
+template <typename R>
+    concept Range = std::ranges::bidirectional_range<R>;
+
+template <typename R, typename T>
+    concept ConvertibleRange = Range<R> && Convertible<std::ranges::range_value_t<R>, T>;
+
+template <typename C, typename T>
+    concept ConvertibleContainer = Container<C> && Convertible<typename C::value_type, T> && Comparable<C>;
