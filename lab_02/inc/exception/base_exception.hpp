0ccc6f14ded08c1c8c20693874aaa0d5aa627358
n0kkster <order.graph@gmail.com>
2025-04-19 19:36:00 +0300

    del: удалил пережитки прошлого

--- lab_02/inc/exception/base_exception.hpp
+++ lab_02/inc/exception/base_exception.hpp
@@ -5,13 +5,6 @@
 
 BaseException::BaseException(const char *info, const std::source_location &loc)
 {
-    // std::stringstream ss;
-    // ss  << "File: "
-    //     << loc.file_name() << '('
-    //     << loc.line() << ':'
-    //     << loc.column() << ") `"
-    //     << loc.function_name() << "`: "
-    //     << info << '\n';
     snprintf(message, EXCEPTION_MESSAGE_SIZE, "File: %s (%d:%d) `%s`: %s\n", loc.file_name(), loc.line(),
              loc.column(), loc.function_name(), info);
 }
