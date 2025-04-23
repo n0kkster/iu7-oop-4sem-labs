b9b0a64ff51de0aa0d455b3eb6b02653b0166844
n0kkster <order.graph@gmail.com>
2025-04-23 11:32:47 +0300

    add: добавил концепт, чтоб отличить сет от не сета

--- lab_02/inc/set/concepts.h
+++ lab_02/inc/set/concepts.h
@@ -8,7 +8,7 @@ template <typename From, typename To>
 concept Convertible = (std::same_as<From, To> || std::convertible_to<From, To>);
 
 template <typename T, typename U>
-concept HasCommon =  requires { typename std::common_type_t<T, U>; };
+concept HasCommon = requires { typename std::common_type_t<T, U>; };
 
 template <typename T>
 concept Assignable = requires(T &lv, T &rv) { lv = rv; };
@@ -29,20 +29,26 @@ concept CopyConstructible = requires(const C &c) {
     { C(c) } -> std::same_as<C>;
 };
 
+template <CopyMoveAssignable T>
+class Set;
+
+template <typename C>
+concept IsSet = std::same_as<Set<typename C::value_type>, C>;
+
 template <typename C>
 concept Container =
     CopyConstructible<C> && std::move_constructible<C> && std::destructible<C> && requires(C c) {
-        typename C::value_type;
-        typename C::reference;
-        typename C::const_reference;
-        typename C::const_iterator;
-        typename C::difference_type;
-        typename C::size_type;
+        typename std::remove_reference_t<C>::value_type;
+        typename std::remove_reference_t<C>::reference;
+        typename std::remove_reference_t<C>::const_reference;
+        typename std::remove_reference_t<C>::const_iterator;
+        typename std::remove_reference_t<C>::difference_type;
+        typename std::remove_reference_t<C>::size_type;
 
-        { c.cbegin() } noexcept -> std::same_as<typename C::const_iterator>;
-        { c.cend() } noexcept -> std::same_as<typename C::const_iterator>;
+        { c.cbegin() } noexcept -> std::same_as<typename std::remove_reference_t<C>::const_iterator>;
+        { c.cend() } noexcept -> std::same_as<typename std::remove_reference_t<C>::const_iterator>;
 
-        { c.size() } noexcept -> std::same_as<typename C::size_type>;
+        { c.size() } noexcept -> std::same_as<typename std::remove_reference_t<C>::size_type>;
         { c.empty() } noexcept -> std::same_as<bool>;
     };
 
@@ -59,7 +65,7 @@ template <typename R>
 concept Range = std::ranges::input_range<R>;
 
 template <typename R, typename T>
-concept ConvertibleRange = Range<R> && Convertible<std::ranges::range_value_t<R>, T> && !Container<R>;
+concept ConvertibleRange = Range<R> && Convertible<std::ranges::range_value_t<R>, T> && !IsSet<R> && !Container<R>;
 
 template <typename C, typename T>
-concept ConvertibleContainer = Container<C> && Convertible<typename C::value_type, T>;
+concept ConvertibleContainer = !IsSet<C> && Container<C> && Convertible<typename std::remove_reference_t<C>::value_type, T>;
