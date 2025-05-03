d96480d6e95672775508c47ac4fd26ecf3420a9a
n0kkster <order.graph@gmail.com>
2025-05-03 22:20:00 +0300

    add: добавил интерфейс для класса точки

---
+++ lab_03/component/primitive/visible/model/carcass/vertex/vertex.h
@@ -0,0 +1,52 @@
+#pragma once
+
+class Vertex
+{
+private:
+    double m_x, m_y, m_z;
+
+public:
+#pragma region Constructors
+    Vertex() = default;
+    Vertex(const double x, const double y, const double z);
+#pragma endregion
+
+#pragma region Destructor
+    ~Vertex() = default;
+#pragma endregion
+
+#pragma region Getters
+    double getX() const;
+    double getY() const;
+    double getZ() const;
+#pragma endregion
+
+#pragma region Setters
+    void setX(const double x);
+    void setY(const double y);
+    void setZ(const double z);
+#pragma endregion
+
+    double calcDistance(const Vertex &other) const noexcept;
+    void transform(const auto &matrix) noexcept;
+
+#pragma region Operators
+    bool operator==(const Vertex &other) const noexcept;
+    bool isEqual(const Vertex &other) const noexcept;
+
+    bool operator!=(const Vertex &other) const noexcept;
+    bool isNotEqual(const Vertex &other) const noexcept;
+
+    Vertex &add(const Vertex &other) noexcept;
+    Vertex &operator+=(const Vertex &other) noexcept;
+
+    Vertex make_sum(const Vertex &other) const;
+    Vertex operator+(const Vertex &other) const;
+
+    Vertex &subtract(const Vertex &other) noexcept;
+    Vertex &operator-=(const Vertex &other) noexcept;
+
+    Vertex make_diff(const Vertex &other) const;
+    Vertex operator-(const Vertex &other) const;
+#pragma endregion
+};
