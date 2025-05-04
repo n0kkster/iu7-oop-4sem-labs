f34bb0fdbd81ff234f01d3d3fcd5342cee91fae8
n0kkster <order.graph@gmail.com>
2025-05-04 21:52:09 +0300

    add: одно действие потерялось по дороге

---
+++ lab_03/transformations/actions/scale/ScaleAction.cpp
@@ -0,0 +1,44 @@
+#include "ScaleAction.h"
+#include "../move/MoveAction.h"
+
+ScaleAction::ScaleAction(const ScaleParams &params) : TransformAction()
+{
+    m_matrix[0][0] = params.getKx();
+    m_matrix[1][1] = params.getKy();
+    m_matrix[2][2] = params.getKz();
+}
+
+ScaleAction::ScaleAction(const double k) : TransformAction()
+{
+    m_matrix[0][0] = k;
+    m_matrix[1][1] = k;
+    m_matrix[2][2] = k;
+}
+
+ScaleAction::ScaleAction(const Vertex &vertex, const ScaleParams &params) : TransformAction()
+{
+
+    MoveAction to_center({ -vertex.getX(), -vertex.getY(), -vertex.getZ() });
+    m_matrix *= to_center.getMatrix();
+
+    ScaleAction scale(params);
+    m_matrix *= scale.getMatrix();
+
+    MoveAction from_center({ vertex.getX(), vertex.getY(), vertex.getZ() });
+    m_matrix *= from_center.getMatrix();
+}
+
+ScaleAction::ScaleAction(const Vertex &vertex, const double k) : TransformAction()
+{
+
+    MoveAction to_center({ -vertex.getX(), -vertex.getY(), -vertex.getZ() });
+    m_matrix *= to_center.getMatrix();
+
+    ScaleAction scale(k);
+    m_matrix *= scale.getMatrix();
+
+    MoveAction from_center({ vertex.getX(), vertex.getY(), vertex.getZ() });
+    m_matrix *= from_center.getMatrix();
+}
+
+bool ScaleAction::isMoveAction() const noexcept { return false; }
---
+++ lab_03/transformations/actions/scale/ScaleAction.h
@@ -0,0 +1,21 @@
+#ifndef SCALEACTION_H
+#define SCALEACTION_H
+
+#include "../../params/scale/ScaleParams.h"
+#include "../TransformAction.h"
+
+class ScaleAction : public TransformAction
+{
+public:
+    ScaleAction() = default;
+
+    ScaleAction(const ScaleParams &params);
+    ScaleAction(const double k);
+
+    ScaleAction(const Vertex &vertex, const ScaleParams &params);
+    ScaleAction(const Vertex &vertex, const double k);
+
+    bool isMoveAction() const noexcept override;
+};
+
+#endif /* SCALEACTION_H */
