5b049dd4ffd76a24645f0832a6df0c15618ea7fe
n0kkster <order.graph@gmail.com>
2025-05-06 22:51:22 +0300

    add: добавил базовую камеру

--- lab_03/component/primitive/invisible/camera/BaseCamera.h
+++ lab_03/component/primitive/invisible/camera/BaseCamera.h
@@ -1,5 +1,19 @@
-#ifndef BASECAMERA_H
-#define BASECAMERA_H
+#pragma once
 
+#include "../../../../matrix/matrix.h"
+#include "../../../../transformations/actions/TransformAction.h"
+#include "../InvisibleObject.h"
 
-#endif /* BASECAMERA_H */
+#include <memory>
+
+class BaseCamera : public InvisibleObject
+{
+public:
+    BaseCamera();
+    virtual ~BaseCamera() = default;
+
+    virtual const Matrix<double> &getOrientation() const noexcept = 0;
+    virtual void setOrientation(const Matrix<double> &orientation) = 0;
+    virtual void setViewpoint(const Vertex &viewpoint) noexcept = 0;
+    virtual void transform(const std::shared_ptr<const TransformAction> action) = 0;
+};
