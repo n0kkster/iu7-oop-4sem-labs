aa2d11897b0624f465e03e8144e60055d4ffa319
n0kkster <order.graph@gmail.com>
2025-05-07 13:53:24 +0300

    add: заглушки для остальных ридеров

---
+++ lab_03/reader/model/carcass/CarcassReader.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../ModelReader.h"
+
+class CarcassReader : public ModelReader
+{
+public:
+    CarcassReader() = delete;
+    CarcassReader(const std::string &filename) : ModelReader(filename) { };
+
+    virtual ~CarcassReader() = default;
+};
