65acd30e1d51600e650dad19d7b751be955ef6fa
n0kkster <order.graph@gmail.com>
2025-05-14 19:17:58 +0300

    add: поменял цвет рисования

--- lab_03/factories/draw/qt/products/QtPainter.cpp
+++ lab_03/factories/draw/qt/products/QtPainter.cpp
@@ -1,15 +1,15 @@
 #include "QtPainter.h"
 
-QtPainter::QtPainter(std::unique_ptr<QGraphicsScene> scene) : m_scene(std::move(scene)) { }
+QtPainter::QtPainter(std::shared_ptr<QGraphicsScene> scene) : m_scene(scene) { }
 
 void QtPainter::drawLine(const Vertex &p1, const Vertex &p2)
 {
-    m_scene->addLine(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+    m_scene->addLine(p1.getX(), p1.getY(), p2.getX(), p2.getY(), {Qt::white});
 }
 
 void QtPainter::drawLine(const double x1, const double y1, const double x2, const double y2)
 {
-    m_scene->addLine(x1, y1, x2, y2);
+    m_scene->addLine(x1, y1, x2, y2, {Qt::white});
 }
 
 void QtPainter::clear()
