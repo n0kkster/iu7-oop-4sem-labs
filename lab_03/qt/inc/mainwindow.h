ad723407db1458d1bc8aca14ab682368bf298157
n0kkster <order.graph@gmail.com>
2025-05-14 19:23:34 +0300

    upd: начал вязать прогу к интерфейсу. уже умеем рисоваться

--- lab_03/qt/inc/mainwindow.h
+++ lab_03/qt/inc/mainwindow.h
@@ -1,13 +1,19 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "../../facade/Facade.h"
+
+#include <memory>
+#include <QGraphicsScene>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
+
 namespace Ui
 {
     class MainWindow;
 }
+
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -19,11 +25,16 @@ public:
     ~MainWindow();
 
 private slots:
+    void onLoadModelBtnClicked();
     void onShiftBtnClicked();
     void onScaleBtnClicked();
     void onRotateBtnClicked();
 
 private:
     Ui::MainWindow *ui;
+    std::shared_ptr<QGraphicsScene> m_scene;
+    Facade m_facade;
+
+    void createScene(QWidget *parent);
 };
 #endif /* MAINWINDOW_H */
--- lab_03/qt/src/mainwindow.cpp
+++ lab_03/qt/src/mainwindow.cpp
@@ -1,18 +1,65 @@
 #include "../inc/mainwindow.h"
+
+#include "../../commands/camera/CameraCommand.h"
+#include "../../commands/load/LoadCommand.h"
+#include "../../commands/object/ObjectCommand.h"
+#include "../../commands/scene/SceneCommand.h"
+#include "../../exceptions/BaseException.h"
+#include "../../facade/Facade.h"
+#include "../../factories/draw/DrawSolution.h"
+#include "../../factories/draw/qt/QtDrawFactory.h"
+#include "../../ids/ids.h"
+#include "../../managers/ManagerSolution.h"
 #include "../../out/ui_mainwindow.h"
 
+#include <QFileDialog>
 #include <QMessageBox>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
     connect(ui->applyShiftBtn, &QPushButton::clicked, this, &MainWindow::onShiftBtnClicked);
     connect(ui->applyScaleBtn, &QPushButton::clicked, this, &MainWindow::onScaleBtnClicked);
     connect(ui->applyRotateBtn, &QPushButton::clicked, this, &MainWindow::onRotateBtnClicked);
+    connect(ui->loadModelBtn, &QPushButton::clicked, this, &MainWindow::onLoadModelBtnClicked);
+
+    createScene(ui->planeWidget);
+
+    CameraId id = CameraId::DefaultCameraId;
+    std::shared_ptr<BaseCommand> addCameraCommand = std::make_shared<AddCameraCommand>(id);
+    m_facade.execute(addCameraCommand);
+
+    qDebug() << "active cam id:" << id;
+
+    std::shared_ptr<BaseCommand> setActiveCameraCommand = std::make_shared<SetActiveCameraCommand>(id);
+    m_facade.execute(setActiveCameraCommand);
 }
 
+void MainWindow::onLoadModelBtnClicked()
+{
+    QString qFilename = QFileDialog::getOpenFileName(
+        this, "Открыть файл", "", "Текстовые файлы (*.txt);;CSV файлы (*.csv);;OBJ файлы (*.obj)");
+
+    if (qFilename.isEmpty())
+        return;
+
+    std::string filename = qFilename.toStdString();
+
+    InternalRepresentationId repr = static_cast<InternalRepresentationId>(ui->modelReprList->currentIndex());
+
+    try
+    {
+        std::shared_ptr<BaseCommand> loadCommand = std::make_shared<CarcassLoadCommand>(repr, filename);
+        m_facade.execute(loadCommand);
+        std::shared_ptr<BaseCommand> drawCommand = std::make_shared<DrawSceneCommand>();
+        m_facade.execute(drawCommand);
+    }
+    catch (const std::exception &ex)
+    {
+        QMessageBox::critical(this, "Error!", ex.what());
+    }
+}
 
 void MainWindow::onShiftBtnClicked()
 {
@@ -38,7 +85,7 @@ void MainWindow::onShiftBtnClicked()
     {
         QMessageBox::critical(this, "Ошибка", "Величина сдвига по оси Z должна быть вещественным числом.");
         return;
-    }    
+    }
 }
 
 void MainWindow::onScaleBtnClicked()
@@ -50,42 +97,48 @@ void MainWindow::onScaleBtnClicked()
     cx = ui->centerXScale->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Координата X центра масштабирования должна быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Координата X центра масштабирования должна быть вещественным числом.");
         return;
     }
 
     cy = ui->centerYScale->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Координата Y центра масштабирования должна быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Координата Y центра масштабирования должна быть вещественным числом.");
         return;
     }
 
     cz = ui->centerZScale->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Координата Z центра масштабирования должна быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Координата Z центра масштабирования должна быть вещественным числом.");
         return;
     }
 
     kx = ui->kXScale->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Коэффициент масштабирования по оси X должен быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Коэффициент масштабирования по оси X должен быть вещественным числом.");
         return;
     }
 
     ky = ui->kYScale->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Коэффициент масштабирования по оси Y должен быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Коэффициент масштабирования по оси Y должен быть вещественным числом.");
         return;
     }
 
     kz = ui->kZScale->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Коэффициент масштабирования по оси Z должен быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Коэффициент масштабирования по оси Z должен быть вещественным числом.");
         return;
     }
 }
@@ -99,21 +152,24 @@ void MainWindow::onRotateBtnClicked()
     cx = ui->centerXRotate->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Координата X центра вращения должна быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Координата X центра вращения должна быть вещественным числом.");
         return;
     }
 
     cy = ui->centerYRotate->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Координата Y центра вращения должна быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Координата Y центра вращения должна быть вещественным числом.");
         return;
     }
 
     cz = ui->centerZRotate->text().toDouble(&ok);
     if (!ok)
     {
-        QMessageBox::critical(this, "Ошибка", "Координата Z центра вращения должна быть вещественным числом.");
+        QMessageBox::critical(this, "Ошибка",
+                              "Координата Z центра вращения должна быть вещественным числом.");
         return;
     }
 
@@ -141,6 +197,30 @@ void MainWindow::onRotateBtnClicked()
     angleX = qDegreesToRadians(angleX);
     angleY = qDegreesToRadians(angleY);
     angleZ = qDegreesToRadians(angleZ);
+}
+
+#include <QDebug>
+
+void MainWindow::createScene(QWidget *parent)
+{
+    const int w = ui->planeWidget->width();
+    const int h = ui->planeWidget->height();
+
+    auto m_scene = std::make_shared<QGraphicsScene>(0, 0, w, h, parent);
+    m_scene->setSceneRect(QRect{ 0, 0, w, h });
+    m_scene->setBackgroundBrush(QColor::fromRgb(39, 40, 41));
+
+    ui->planeWidget->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    ui->planeWidget->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    ui->planeWidget->setScene(m_scene.get());
+
+    std::unique_ptr<BasePainter> painter = DrawSolution::createPainter<QtDrawFactory>(m_scene);
+    auto drawManager = ManagerSolution::getDrawManager();
+
+    // painter->drawLine(0, 0, 300, 300);
+
+    drawManager->setPainter(std::move(painter));
 }
 
 MainWindow::~MainWindow()
