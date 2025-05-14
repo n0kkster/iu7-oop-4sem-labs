d19f0052f6e6be90a8705b79f35635139979bfeb
n0kkster <order.graph@gmail.com>
2025-05-14 19:23:13 +0300

    add: временно сделал костыльный сдвиг в центр при проекции

--- lab_03/strategies/projection/defaut/DefaultProjectionStrategy.cpp
+++ lab_03/strategies/projection/defaut/DefaultProjectionStrategy.cpp
@@ -10,10 +10,15 @@ void DefaultProjectionStrategy::prepare(std::shared_ptr<const BaseModel> model,
     auto edges = model->getStructure()->getEdges();
     auto transformMatrix = camera->getOrientation();
 
-    for (const Vertex v : vertices)
+    for (const Vertex &v : vertices)
     {
         Vertex projected(v);
         projected.transform(transformMatrix);
+
+        // TEMP TEMP TEMP
+        projected.setX(projected.getX() + 430);
+        projected.setY(-projected.getY() + 430);
+        // TEMP TEMP TEMP
         m_vertices.push_back(projected);
     }
 
