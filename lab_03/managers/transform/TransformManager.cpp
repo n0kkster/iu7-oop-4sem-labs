c296caf6768ee55f45dc503f5e9187ddba571ae4
n0kkster <order.graph@gmail.com>
2025-05-16 02:02:00 +0300

    upd: статика и тут и там

--- lab_03/managers/transform/TransformManager.cpp
+++ lab_03/managers/transform/TransformManager.cpp
@@ -1,5 +1,5 @@
 #include "TransformManager.h"
-#include "../../visitors/creators/ConcreteVisitorCreator.h"
+#include "../../visitors/creators/VisitorCreator.h"
 #include "../ManagerSolution.h"
 
 void TransformManager::moveObject(size_t id, const MoveParams &params)
@@ -7,8 +7,7 @@ void TransformManager::moveObject(size_t id, const MoveParams &params)
     auto sceneManager = ManagerSolution::getSceneManager();
     auto object = sceneManager->getObject(id);
 
-    MoveVisitorCreator creator;
-    auto visitor = creator.create(params);
+    auto visitor = MoveVisitorCreator::create(params);
     object->accept(visitor);
 }
 
@@ -17,8 +16,7 @@ void TransformManager::scaleObject(size_t id, const ScaleParams &params)
     auto sceneManager = ManagerSolution::getSceneManager();
     auto object = sceneManager->getObject(id);
 
-    ScaleVisitorCreator creator;
-    auto visitor = creator.create(params);
+    auto visitor = ScaleVisitorCreator::create(params);
     object->accept(visitor);
 }
 
@@ -27,7 +25,6 @@ void TransformManager::rotateObject(size_t id, const RotationParams &params)
     auto sceneManager = ManagerSolution::getSceneManager();
     auto object = sceneManager->getObject(id);
 
-    RotateVisitorCreator creator;
-    auto visitor = creator.create(params);
+    auto visitor = RotateVisitorCreator::create(params);
     object->accept(visitor);
 }
