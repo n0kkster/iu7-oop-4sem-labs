d6061388dd8312a3cf7698f7fc79e7390b04e6d5
n0kkster <order.graph@gmail.com>
2025-05-18 16:21:51 +0300

    upd: убрал мув

--- lab_03/factories/draw/DrawSolution.hpp
+++ lab_03/factories/draw/DrawSolution.hpp
@@ -5,7 +5,7 @@
 template <typename Tfactory, typename Tscene>
 std::unique_ptr<BasePainter> DrawSolution::createPainter(std::shared_ptr<Tscene> scene)
 {
-    std::shared_ptr<BaseDrawFactory> factory = std::make_shared<Tfactory>(std::move(scene));
+    std::shared_ptr<BaseDrawFactory> factory = std::make_shared<Tfactory>(scene);
 
     return factory->createPainter();
 }
